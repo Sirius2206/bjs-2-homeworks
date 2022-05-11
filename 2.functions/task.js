86d69b73c5ba5b9eaaec1797d6ef021024c2722e
sirius2206 <sergei_galkin@inbox.ru>
2022-05-11 18:56:52 +0500

    Functions homework

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -1,31 +1,41 @@
 // Задание 1
 function getArrayParams(arr) {
-  let min, max, sum, avg;
+  let min = arr[0];
+  let max = arr[0];
+  let sum = 0;
+  arr.forEach(el => {
+    min = (el < min) ? el : min;
+    max = (el > max) ? el : max;
+    sum += el;
+  });
 
-  // Ваш код
-
+  let avg = +((sum / arr.length).toFixed(2));
   return { min: min, max: max, avg: avg };
 }
 
 // Задание 2
 function worker(arr) {
-  let sum;
-
-  // Ваш код
-
-  return sum;
+  return arr.reduce((sum, cur) => sum + cur, 0);
 }
 
 function makeWork(arrOfArr, func) {
-  let max;
-
-  // Ваш кода
-  // for ...
-  
+  let max = -Infinity;
+  let sum = 0;
+  arrOfArr.forEach(el => {
+    sum = func(el);
+    max = (sum > max) ? sum : max;
+  });
   return max;
 }
 
 // Задание 3
 function worker2(arr) {
-  // Ваш код
+  let min = arr[0];
+  let max = arr[0];
+  arr.forEach(el => {
+    min = (el < min) ? el : min;
+    max = (el > max) ? el : max;
+  });
+
+  return max - min;
 }
