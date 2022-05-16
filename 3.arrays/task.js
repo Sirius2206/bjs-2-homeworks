919af1b4c2732c679eda99ba20e2c47a367d8c71
sirius2206 <sergei_galkin@inbox.ru>
2022-05-16 09:15:26 +0500

    Homework 3 Arrays

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,15 +1,8 @@
 function compareArrays(arr1, arr2) {
-  let result;
+  return arr1.length === arr2.length && arr1.every((elem, index) => elem === arr2[index]);
 
-  // Ваш код
-
-  return result; // boolean
 }
 
 function advancedFilter(arr) {
-  let resultArr;
-
-  // Ваш код
-
-  return resultArr; // array
+  return resultArr = arr.filter(x => (x > 0 && x % 3 === 0)).map(x => x * 10);
 }
