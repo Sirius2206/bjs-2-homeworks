584154e63804dcd0b0d9f20e8abb02449b90712f
sirius2206 <sergei_galkin@inbox.ru>
2022-05-18 19:38:51 +0500

    Homework 4 Objects

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,10 +1,41 @@
 function Student(name, gender, age) {
-    // Ваш код
+    this.name = name;
+    this.gender = gender;
+    this.age = age;
 
 }
 
 Student.prototype.setSubject = function (subjectName) {
-  //ваш код
+  this.subject = subjectName;
 }
 
-// ваш код для остальных методов
+Student.prototype.addMark = function (mark) {
+  if (this.marks === undefined) {
+    this.marks = [];
+  }
+  this.marks.push(mark);
+}
+
+Student.prototype.addMarks = function(...all){
+  if (this.marks === undefined) {
+    this.marks = [];
+  }
+  all.forEach(mark => {
+    this.marks.push(mark)
+  });
+}
+
+Student.prototype.getAverage = function() {
+  if (this.marks === undefined) return 0;
+  let sum = 0;
+  this.marks.forEach(mark => {
+    sum += mark;
+  })
+  return sum / this.marks.length;
+}
+
+Student.prototype.exclude = function(reason) {
+  delete(this.marks);
+  delete(this.subject);
+  this.excluded = reason;
+}
