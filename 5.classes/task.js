5ff4f5240512b1130dda23b6cf16d90c08c768d4
Sirius2206 <sergei_galkin@inbox.ru>
2022-05-21 02:16:05 +0500

    Homework 5 classes

--- 5.classes/task.js
+++ 5.classes/task.js
@@ -1,9 +1,9 @@
 class PrintEditionItem {
-    constructor (name, releaseDate, pagesCount, newState = 100) {
+    constructor (name, releaseDate, pagesCount, state = 100) {
         this.name = name;
         this.releaseDate = releaseDate;
         this.pagesCount = pagesCount;
-        this.state = newState;
+        this._state = state;
         this.type = null;
     }
 
@@ -11,8 +11,8 @@ class PrintEditionItem {
         this.state = this.state * 1.5;
     }
 
-    set state (newState) {
-        this._state = (newState < 0) ? 0 : (newState > 100) ? 100 : newState;
+    set state (state) {
+        this._state = (state < 0) ? 0 : (state > 100) ? 100 : state;
 
     }
 
@@ -23,53 +23,35 @@ class PrintEditionItem {
 
 class Magazine extends PrintEditionItem {
     constructor (name, releaseDate, pagesCount, state = 100) {
-        this.name = name;
-        this.releaseDate = releaseDate;
-        this.pagesCount = pagesCount;
-        this.state = state;
+       super(name, releaseDate, pagesCount, state = 100);
         this.type = "magazine";
     }
 }
 
 class Book extends PrintEditionItem {
     constructor (author, name, releaseDate, pagesCount, state = 100) {
+        super(name, releaseDate, pagesCount, state = 100);
         this.author = author;
-        this.name = name;
-        this.releaseDate = releaseDate;
-        this.pagesCount = pagesCount;
-        this.state = state;
         this.type = "book";
     }
 }
 class NovelBook extends Book {
     constructor (author, name, releaseDate, pagesCount, state = 100) {
-        this.author = author;
-        this.name = name;
-        this.releaseDate = releaseDate;
-        this.pagesCount = pagesCount;
-        this.state = state;
+        super(author, name, releaseDate, pagesCount, state = 100);
         this.type = "novel";
     }
 }
 
 class FantasticBook extends Book {
     constructor (author, name, releaseDate, pagesCount, state = 100) {
-        this.author = author;
-        this.name = name;
-        this.releaseDate = releaseDate;
-        this.pagesCount = pagesCount;
-        this.state = state;
+        super(author, name, releaseDate, pagesCount, state = 100);
         this.type = "fantastic";
     }
 }
 
 class DetectiveBook extends Book {
     constructor (author, name, releaseDate, pagesCount, state = 100) {
-        this.author = author;
-        this.name = name;
-        this.releaseDate = releaseDate;
-        this.pagesCount = pagesCount;
-        this.state = state;
+        super(author, name, releaseDate, pagesCount, state = 100);
         this.type = "detective";
     }
 }
@@ -92,16 +74,86 @@ class Library {
         let result = null;
         this.books.forEach(book => {
             if (book[type] === value) {
-                return book;
+                result = book;
+                return result;
             }
         })
+        return result;
     }
 
     giveBookByName (bookName) {
         for (let i = 0; i < this.books.length; i++) {
             if (this.books[i].name === bookName) {
-                return this.books.splice(i, 1);
+                let result = this.books[i];
+                this.books.splice(i, 1);
+                return result;
             }
         }
+        return null;
     }
 }
+
+
+class Student {
+    constructor (name) {
+        this.name = name;
+    }
+
+    disciplines = {};
+
+    addMark (mark, subject) {
+        if (!this.disciplines?.[subject]) {
+            this.disciplines[subject] = [];
+        }
+        if (mark < 1 || mark > 5) {
+            console.log("Ошибка, оценка должна быть числом от 1 до 5");
+            return;
+        }
+        this.disciplines[subject].push(mark);
+    }
+
+    getAverageBySubject (subject) {
+        if (!this.disciplines?.[subject]) {
+            console.log ("Несуществующий предмет");
+            return;
+        }
+        let sum = 0;
+        this.disciplines[subject].forEach(el => {
+            sum += el;
+        })
+        return sum / this.disciplines[subject].length;
+    }
+
+    getAverage () {
+        let sum = 0;
+        let marksCount = 0;
+        for (let sub in this.disciplines) {
+            this.disciplines[sub].forEach (el => {
+                sum += el;
+                marksCount++;
+            })
+        }
+        return sum / marksCount;
+    }
+
+    printGrades () {
+        for (let sub in this.disciplines) {
+            console.log(`Оценки по предмету ${sub}: ${this.disciplines[sub]}`);
+        }
+    }
+
+    exclude (reason) {
+        delete(this.disciplines);
+        this.excluded = reason;
+    }
+}
+
+let student = new Student("Иван Петров");
+student.addMark(3, "algebra");
+student.addMark(5, "algebra");
+student.addMark(3, "algebra");
+      student.addMark(5, "algebra");
+      student.addMark(5, "history");
+      student.addMark(5, "history");
+
+    student.printGrades();
