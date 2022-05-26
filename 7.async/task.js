38573bf4601cff222bf3d8339bd2361ddc86ff4e
sirius2206 <sergei_galkin@inbox.ru>
2022-05-26 10:22:25 +0500

    Homework 7 async

--- 7.async/task.js
+++ 7.async/task.js
@@ -1,16 +1,19 @@
 class AlarmClock {
-    alarmCollection = [];
-    timerId = null;
+        alarmCollection = [];
+        timerId = null;
+        isPaused = false;
 
-    addClock (id, time, callback) {
+    addClock (time, callback, id) {
         if (id === undefined) {
             throw new Error ("Отсутствует идентификатор создаваемого звонка");
         }
-        if (this.alarmCollection.some((obj) => {obj.id === id})) {
+
+        let check = this.alarmCollection.some((obj) => obj.id === id);
+        if (check) {
             console.error("Будильник с заданным id уже существует");
             return;
         }
-        let resultClock = {
+        const resultClock = {
             id: id,
             time: time,
             callback: callback
@@ -19,32 +22,60 @@ class AlarmClock {
     }
 
     removeClock (id) {
-        let result = this.alarmCollection.filter((obj) => {obj.id !== id})
-        if (result.length > 0) {
+        const prevLength = this.alarmCollection.length;
+        this.alarmCollection = this.alarmCollection.filter((obj) => {obj.id !== id;})
+        if (this.alarmCollection.length < prevLength) {
             console.log(`Будильник с id = ${id} удален.`)
             return;
         }
         console.log("Не удалось найти будильник с таким id");
+        return;
     }
 
     getCurrentFormattedTime () {
-        let date = new Date();
-        let result = `${date.getHours}:${date.getMinutes}`;
+        const dateStr = this.getFormattedTime(new Date());
+        return dateStr;
+    }
+
+    getFormattedTime(date) {
+        const hour = (date.getHours() < 10) ? '0' + date.getHours() : date.getHours();
+        const minute = (date.getMinutes() < 10) ? '0' + date.getMinutes() : date.getMinutes();
+        const result = `${hour}:${minute}`;
         return result;
     }
 
-    start() {
+    checkClock(clock) {
+        if (clock.time === this.getCurrentFormattedTime()) {
+            this.isPaused = true;
+            let alarm = setInterval(() => clock.callback(), 1000);
+            setTimeout(() => {clearInterval(alarm); this.isPaused = false;}, 60000);
+        }
+    }
 
+    start() {
+        this.timerId = setInterval(() => {
+            if (!this.isPaused) {
+                this.alarmCollection.forEach((alarm) => this.checkClock(alarm))
+            }
+        }, 1000);
     }
 
     stop() {
-
+        if (this.timerId) {
+            clearInterval(this.timerId);
+            this.timerId = null;
+        }
     }
 
     printAlarms() {
         console.log(`Печать всех будильников в количестве: ${this.alarmCollection.length}`);
-        this.alarmCollection.forEach((el) => {
-            console.log(`Будильник №${el.id} заведен на ${el.time}`);   
+        this.alarmCollection.forEach((alarm) => {
+            console.log(`Будильник №${alarm.id} заведен на ${alarm.time}`);   
         })
+    }
+
+    clearAlarms() {
+        clearInterval(this.timerId);
+        this.alarmCollection.length = 0;
     }
 }
